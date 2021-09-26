d489edb7aa7ccff46b6fe90b6bc9062b847ea188
ocube-leroy <olagokeolowu@gmail.com>
2021-09-27 00:46:44 +0800

    arrays.js file

---
+++ arrays.js
@@ -0,0 +1,13 @@
+
+    function colorsArray(myArray){
+        const myArray = ["yellow", "blue", "purple", "lilac", "peach", "red", "black"];
+        const colorsArrayLength = myArray.length();
+        const middleArrayColor = ((myArray.length-1)/2);
+    const middleArrayIndex = myArray[index];
+
+    console.log(colorsArrayLength);
+    console.log(middleArrayColor);
+    console.log(middleArrayIndex);
+}
+    
+    colorsArray("yellow", "blue", "purple", "lilac", "peach", "red", "black");
